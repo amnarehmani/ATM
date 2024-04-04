862ae5f00115b1ea27d7944df8ba50a3f59b28ba
amnarehmani <jeonamnaaa@gmail.com>
2024-04-04 20:48:19 +0500

    ATM

---
+++ index.js
@@ -0,0 +1,41 @@
+import inquirer from "inquirer";
+let MyBalance = 20000;
+let myPin = 2323;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "Pin",
+        message: "Enter You Pin",
+        type: "number",
+    },
+]);
+if (pinAnswer.Pin === myPin) {
+    console.log("correct pin code!");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "please select an option",
+            type: "list",
+            choices: ["withdraw", "check balance"],
+        },
+    ]);
+    if (operationAns.operation === "withdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "enter your amount",
+                type: "number",
+            },
+        ]);
+        if (MyBalance <= amountAns.amount)
+            console.log("you don't have enough amount to withdraw");
+        if (MyBalance -= amountAns.amount) {
+            console.log("your current balance :" + MyBalance);
+        }
+    }
+    else if (operationAns.operation === "check balance") {
+        console.log("your balance is:" + MyBalance);
+    }
+}
+else {
+    console.log("incorrect pin code");
+}
